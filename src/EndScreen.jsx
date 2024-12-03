e8329c4e29fe1e8bfebc2f2265c6eb84b62700cb
Rebecca Arkhed Stationär <rebar310@student.liu.se>
2024-12-03 15:37:23 +0100

    Styling på slutskäärmen

--- src/EndScreen.jsx
+++ src/EndScreen.jsx
@@ -22,7 +22,7 @@ function EndScreen() {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        padding: "20px",
+        //padding: "20px",
         height: "100vh",
         justifyContent: "center",
         backgroundImage: "url('src/assets/tillfalliggameoverbakgrund.PNG')", //tillfällig bakgrundsbild tills jag fått bättre av Aya :)
@@ -31,8 +31,18 @@ function EndScreen() {
         /* color: "white", */
       }}
     >
+      <style>
+        {" "}
+        {/* Detta finns till endast för att fixa en cool font :) */}
+        {`
+          @font-face {
+            font-family: 'PixelFont';
+            src: url('src/assets/pixeboy-font/Pixeboy-z8XGD.ttf') format('truetype');
+          }
+        `}
+      </style>
       {/* Om vi vill ha någon mer text eller knapp på slutskärmen läggs detta till här */}
-      <p style={timestyle}>Din tid: {elapsedTime}s</p>
+      <p style={timestyle}>Din tid: {elapsedTime} s</p>
       <p style={scorestyle}>Ditt score: {score}</p>
       <button onClick={handleBackToStart} style={buttonStyle}>
         Tillbaka till Startskärmen
@@ -43,23 +53,29 @@ function EndScreen() {
 
 // Stylar texten "Din tid: .. s"
 const timestyle = {
+  fontFamily: "PixelFont", // Samma font som Course Slayer
+  //textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+  fontSize: "50px",
   color: "black",
-  marginTop: "200px",
+  marginTop: "300px",
 };
 
 // Stylar texten "Ditt score"
 const scorestyle = {
+  fontFamily: "PixelFont", // Samma font som Course Slayer
+  //textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+  fontSize: "50px",
   color: "black",
-  marginTop: "1px",
+  marginTop: "-40px",
 };
 
 // Styling för knappen "Tillbaka till startskärmen"
 const buttonStyle = {
   marginTop: "5px", // Avståndet från knappen "Tillbaka till startskärmmen" upp till texten "ditt score"
   padding: "10px 20px",
-  fontSize: "16px",
+  fontSize: "20px",
   cursor: "pointer",
-  backgroundColor: "#4CAF50",
+  backgroundColor: "#383838",
   color: "white",
   border: "none",
   borderRadius: "4px",
